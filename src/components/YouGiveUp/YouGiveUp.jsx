91c5ee101d08ba2e4ce059b1481196640f279b56
Eric <ericdelmermillen@gmail.com>
2023-08-26 17:08:54 -0400

    basic implementation of YouGiveUp component and conditional render finished

---
+++ src/components/YouGiveUp/YouGiveUp.jsx
@@ -0,0 +1,18 @@
+import "./YouGiveUp.scss";
+
+const YouGiveUp = () => {
+  return (
+    <div className="youGiveUp__card">
+
+      <h1 className="youGiveUp__heading">You Give Up!</h1>
+
+      <p className="youGiveUp__question">Gab: </p>
+
+      <p className="youGiveUp__answer">Answer: </p>
+
+      <p className="youGiveUp__points">Points Earned: </p>
+
+    </div>
+  )};
+
+export default YouGiveUp;
