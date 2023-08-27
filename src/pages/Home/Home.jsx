1d3fb88887985c98c479ff2d56848711d59cef9a
Eric <ericdelmermillen@gmail.com>
2023-08-27 14:27:04 -0400

    added the react npm package toastify and added a toast to the home screen if the user tries to play without selecting a level

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Button from "../../components/Button/Button";
 import { useSpeechRecognition } from 'react-speech-recognition';
 import "./Home.scss";
+
+
+import { Flip, Slide, ToastContainer, Zoom, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Home = () => {
   const { browserSupportsSpeechRecognition } = useSpeechRecognition();
 
   const [level, setLevel] = useState("");
+
+  const showToast = () => {
+    // toast("😒 Please select a Level first buddy...")
+    toast("🙄 Please select a Level first...")
+  }
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Sorry, this browser doesn't support speech recognition.</span>;
@@ -20,6 +28,7 @@ const Home = () => {
 
   return (
       <div className="home">
+
         <h2 className="home__heading">"It’s Not What You Say… <br></br>...It’s how you say it"</h2>
         <h3 className="home__sub-heading">Choose your level: </h3>
         <div className="level__links">
@@ -71,15 +80,31 @@ const Home = () => {
             <Link className="button--play" 
             to={`/${level}`}
             >
-              Select
+              Play
             </Link>
           :
             <Link className="button--play-disabled" 
-            onClick={() => alert("Choose a level first!")}
+            onClick={showToast }
             >
-              Select
+              Play
             </Link>
           }
+          <ToastContainer 
+            autoClose={2000}
+            closeOnClick
+            draggable
+            hideProgressBar={true}
+            newestOnTop={false}
+            pauseOnFocusLoss={false}
+            pauseOnHover={false}
+            position="bottom-center"
+            theme="light"
+            className="not-selected__toast"
+            bodyStyle={{color: "#333"}}
+            // transition={Zoom}
+            transition={Flip}
+          />
+
         </div>
 
       </div>
