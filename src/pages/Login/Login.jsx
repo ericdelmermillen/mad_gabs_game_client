27a3bcf29a637f14eebe7cbb12f9f6b6964db59f
Eric <ericdelmermillen@gmail.com>
2023-09-03 10:35:00 -0400

    basic styling for Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,49 +1,44 @@
+import React from 'react';
 import "./Login.scss";
 
 import Google from "../../assets/img/google.png";
-// import Facebook from "../../assets/img/facebook.png";
 
 const Login = () => {
   const google = () => {
     window.open("http://localhost:5000/auth/google", "_self");
   };
 
-  // const facebook = () => {
-  //   window.open("http://localhost:5000/auth/facebook", "_self");
-  // };
-
   return (
     <div className="login">
-
       <h1 className="login__title">Choose a Login Method</h1>
-      
       <div className="login__inner-container">
-      
-        <div className="left">
-      
+        <div className="inner-container__left">
           <div className="login__button login__button--google" onClick={google}>
-      
-            <img src={Google} alt="google icon"className="login__button-icon" />
+            <img src={Google} alt="google icon" className="login__button-icon" />
             Google
-
           </div>
-
-          {/* <div className="login__button login__button--facebook" onClick={facebook}>
-            <img src={Facebook} alt="facebook icon" className="login__button-icon" />
-            Facebook
-          </div> */}
-
         </div>
-
         <div className="or">
           <div className="or__line" />
           <div className="or__text">OR</div>
         </div>
+        <div className="inner-container__right">
 
-        <div className="right">
-          <input type="text" placeholder="Username" />
-          <input type="text" placeholder="Password" />
-          <button className="submit">Login</button>
+          <form className="login-form" action="submit">
+          
+            <label className='login-form__label form__label--email' htmlFor="email">Email</label>
+          
+            <input className="login-form__input" type="emailt" id="email" placeholder="Email"/>
+          
+            <label className='login-form__label form__label--password' htmlFor="password">Password</label>
+          
+            <input className="login-form__input" type="password" id="password" placeholder="Password"/>
+          
+            <button className="login-form__submit">Login</button>
+          </form>
+
+          <span className='signup'>Don't have an account?</span>
+
         </div>
       </div>
     </div>
